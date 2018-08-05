e888afbf8953cff4092c790213eb13dd8cb36d75
Gevorg <araqelyangevorg@gmail.com>
2018-08-05 11:10:07 +0400

    2x_perf how to break dependency for loops and gain more than 2x.

---
+++ benchmarking/2x_perf.cpp
@@ -0,0 +1,53 @@
+#include <benchmark/benchmark.h>
+#include <vector>
+
+
+void bm_loop_x(benchmark::State& state)
+{
+	std::vector<float> v1(16024, 1.021), v2(16024, 3.401);
+	volatile int num = 20;
+	volatile float d;
+	for (auto _ : state)
+	{
+		for (auto coeff = 0; coeff < num; ++coeff)
+		{
+			for (unsigned long long sample = 0; sample < v1.size() - num; sample++)
+				v2[coeff] += v1[sample] + v1[sample + coeff];
+		}
+		//for (auto coeff = 0; coeff < num; ++coeff)
+		//{
+		//	auto d = 0.0f;
+		//	for (unsigned long long sample = 0; sample < v1.size() - num; sample++)
+		//		d += v1[sample] + v1[sample + coeff];
+		//	v2[coeff] = d;
+		//}
+		benchmark::DoNotOptimize(v1);
+		benchmark::ClobberMemory();
+	}
+	benchmark::DoNotOptimize(state.iterations());
+	state.SetLabel("loopx__time");
+}
+
+void bm_loop_more_than_2x(benchmark::State& state)
+{
+	std::vector<float> v1(16024, 1.021), v2(16024, 3.401);
+	volatile int num = 20;
+	volatile float d;
+	for (auto _ : state)
+	{
+		for (auto coeff = 0; coeff < num; ++coeff)
+		{
+			auto d = 0.0f;
+			for (unsigned long long sample = 0; sample < v1.size() - num; sample++)
+				d += v1[sample] + v1[sample + coeff];
+			v2[coeff] = d;
+		}
+		benchmark::DoNotOptimize(v1);
+		benchmark::ClobberMemory();
+	}
+	benchmark::DoNotOptimize(state.iterations());
+	state.SetLabel("loop2x__time");
+}
+BENCHMARK(bm_loop_more_than_2x);
+BENCHMARK(bm_loop_x);
+
--- benchmarking/iterable.hpp
+++ benchmarking/iterable.hpp
@@ -1,8 +1,27 @@
-#include <benchmark/benchmark.h>
-#include <vector>
+#pragma once
+
 #include <random>
-static std::uniform_int_distribution<> rand_engine;
-static std::mt19937_64 gen;
+
+template<class It>
+class iterable
+{
+	It m_first_, m_last_;
+public:
+	iterable() = default;
+	iterable(It first, It last) :m_first_(first), m_last_(last)
+	{}
+	It begin() const { return m_first_; }
+	It end() const { return m_last_; }
+};
+
+template<class It>
+inline iterable<It> make_iterable(It a, It b)
+{
+	return iterable<It>(a, b);
+}
+
+std::uniform_int_distribution<> rand_engine;
+std::mt19937_64 gen;
 template<class T>
 struct RNG
 {
@@ -56,36 +75,3 @@ struct RNG
 	range_iterator begin() const { return range_iterator{ count,first,last }; }
 	range_iterator end() const { return range_iterator{ 0,first,last }; }
 };
-
-
-static void cachBench(benchmark::State& s)
-{
-	const int bytes = 1 << s.range(0);
-	const int count = (bytes / (sizeof(int))) / 2;
-	std::vector<int> v(count);
-	for (auto i : RNG<int>(std::numeric_limits<int>::min(),
-		std::numeric_limits<int>::max(), count))
-	{
-		v.push_back(i);
-	}
-	std::vector<int> indices;
-	for (auto i : RNG<int>(0, count, count))
-	{
-		indices.push_back(i);
-	}
-	for (auto _ : s)
-	{
-		long sum = 0;
-		for (int i : indices)
-			sum += v[i];
-		benchmark::DoNotOptimize(sum);
-	}
-	s.SetBytesProcessed(s.iterations()* bytes);
-	s.SetLabel(std::to_string(bytes / 1024) + "__kb");
-
-}
-
-//
-//BENCHMARK(cachBench)->DenseRange(13, 27)->ReportAggregatesOnly(true);
-//
-//BENCHMARK_MAIN();
